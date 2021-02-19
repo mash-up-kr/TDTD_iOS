d2857f9af66f2c2b12bfab59c4c60b08314e3618
HochanLee <1ghcks@naver.com>
2021-02-19 23:25:59 +0900

    first commit

---
+++ TDTD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDTD
+//
+//  Created by Hochan Lee on 2021/02/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
