2361bcb8fd2f9ff268ad3a7ac77a8021716184cf
namsoo5 <are34@naver.com>
2021-02-27 23:46:14 +0900

    UPDATE: 음성녹음시 현재 상태에따른 분기처리

--- TDTD/RollingpaperWriteViewModel.swift
+++ TDTD/RollingpaperWriteViewModel.swift
@@ -7,15 +7,68 @@
 
 import Foundation
 
-class RollingpaperWriteViewModel {
+class RollingpaperWriteViewModel: ObservableObject {
     enum WriteMode {
         case text, voice
     }
+    enum RecordStatus {
+        case none, record, end, play, pause
+    }
     
     let mode: WriteMode
+    @Published private var recordStatus: RecordStatus = .none
+    var recordDescription: String {
+        switch recordStatus {
+        case .none:
+            return "버튼을 눌러 녹음하기"
+        case .record:
+            return "녹음하고 있어요!"
+        case .play:
+            return "재생하고 있어요!"
+        case .end:
+            return "녹음 들어보기"
+        case .pause:
+            return "다시 재생하기"
+        }
+    }
+    @Published var isEditing = false
     
     init(mode: WriteMode = .text) {
         self.mode = mode
     }
     
+    func record() {
+        recordStatus = .record
+    }
+    
+    func stop() {
+        recordStatus = .end
+    }
+    
+    func play() {
+        recordStatus = .play
+    }
+    
+    func reset() {
+        recordStatus = .none
+    }
+    
+    func pause() {
+        recordStatus = .pause
+    }
+    
+    func recordButtonClick() {
+        switch recordStatus {
+        case .none:
+            record()
+        case .record:
+            stop()
+        case .play:
+            pause()
+        case .end:
+            play()
+        case .pause:
+            play()
+        }
+    }
 }
