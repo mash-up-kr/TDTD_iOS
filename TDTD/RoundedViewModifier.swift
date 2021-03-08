2398bfc9b6804e13a0b25b0ce37cb7a3fd67ee0e
namsoo5 <are34@naver.com>
2021-03-08 16:13:17 +0900

    CREATE: 커스텀가능한 Round ViewModifier 구현
    - 코너 지정가능

---
+++ TDTD/RoundedViewModifier.swift
@@ -0,0 +1,39 @@
+//
+//  RoundedViewModifier.swift
+//  TDTD
+//
+//  Created by 남수김 on 2021/03/08.
+//
+
+import SwiftUI
+
+
+struct RoundedRectangleCustomShape: Shape {
+    let radius: CGFloat
+    let cornerStyle: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect,
+                     byRoundingCorners: cornerStyle,
+                     cornerRadii: CGSize(width: radius, height: radius))
+        
+        
+            
+        return Path(path.cgPath)
+    }
+}
+
+struct RoundedViewModifier: ViewModifier {
+    let radius: CGFloat
+    let cornerStyle: UIRectCorner
+    
+    func body(content: Content) -> some View {
+        content.clipShape(RoundedRectangleCustomShape(radius: radius, cornerStyle: cornerStyle))
+    }
+}
+
+extension View {
+    func cornerRadius(radius: CGFloat, cornerStyle corner: UIRectCorner) -> some View {
+        self.modifier(RoundedViewModifier(radius: radius, cornerStyle: corner))
+    }
+}
