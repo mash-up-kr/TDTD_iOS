c2337adc2bc5788ec21394ec238fd00693f95ce6
namsoo5 <are34@naver.com>
2021-03-08 17:09:59 +0900

    REFACTOR: PlayerView 타입분리

--- TDTD/RollingpaperView.swift
+++ TDTD/RollingpaperView.swift
@@ -18,7 +18,7 @@ struct RollingpaperView: View {
                     isTap.toggle()
                 }
             if isTap {
-                PlayerView(nickName: model.nickName)
+                PlayerView(nickName: model.nickName, roomType: .voice)
             }
         }
     }
@@ -47,55 +47,5 @@ struct RollingpaperModel: Identifiable {
         self.duration = duration
         self.voice = voice
         self.text = text
-    }
-}
-
-struct PlayerView: View {
-    @State private var processValue = 0.3
-    let nickName: String
-    
-    var body: some View {
-        ZStack {
-            // FIXME: - 추후 이미지로 교체 예정인 배경
-            Capsule(style: .circular)
-                .fill(Color.white)
-            Capsule(style: .circular)
-                .stroke()
-                .foregroundColor(Color("beige_3"))
-            VStack {
-                Text(nickName)
-                    .font(Font.uhBeeCustom(14, weight: .bold))
-                    .foregroundColor(Color("grayscale_1"))
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 5)
-                    .background(
-                        Capsule(style: .circular)
-                            .fill(Color("beige_3"))
-                    )
-                    .frame(height: 24)
-                Spacer()
-            }
-            HStack {
-                Image("ic_speak_play_32")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                Spacer().frame(width: 16)
-                VStack {
-                    Spacer().frame(height: 12)
-                    ProgressView(value: processValue)
-                        .accentColor(Color("grayscale_1"))
-                        .frame(width: 120, height: 8)
-                    HStack {
-                        Text("00:00").font(Font.uhBeeCustom(14, weight: .bold))
-                            .foregroundColor(Color("grayscale_3"))
-                        Spacer()
-                        Text("00:03").font(Font.uhBeeCustom(14, weight: .bold))
-                            .foregroundColor(Color("grayscale_3"))
-                    }
-                }
-            }
-            .padding(24)
-        }
-        .frame(width: 224, height: 96)
     }
 }
--- TDTD/SceneDelegate.swift
+++ TDTD/SceneDelegate.swift
@@ -19,7 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = HomeView(viewModel: HomeViewModel())
+//        let contentView = HomeView(viewModel: HomeViewModel())
+        let contentView = RollingpaperView(model: RollingpaperModel(id: "1",
+                                                                    nickName: "tts",
+                                                                    text: "가나다"))
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
