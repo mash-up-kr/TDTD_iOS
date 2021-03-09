8f3c658a1080c5303e75a38a1e6e177caeab58f4
namsoo5 <are34@naver.com>
2021-03-09 16:35:51 +0900

    UPDATE: 롤링페이퍼 상세페이지 그리드UI구현
    - 랜덤위치
    - 랜덤회전
    - 랜덤색상

--- TDTD/RoomDetail/RollingpaperView.swift
+++ TDTD/RoomDetail/RollingpaperView.swift
@@ -9,17 +9,45 @@ import SwiftUI
 
 struct RollingpaperView: View {
     let model: RollingpaperModel
-    @State private var isTap: Bool = true
+    @State private var isTap: Bool = false
+    var randomHorizontalSpacing: CGFloat {
+        CGFloat(Int.random(in: -15...16))
+    }
+    var randomVerticalSpacing: CGFloat {
+        CGFloat(Int.random(in: -8...8))
+    }
+    var randomRotate: Double {
+        Double(Int.random(in: -10...10))
+    }
+    
+    func randomImage(isSelect: Bool) -> Image {
+        if isSelect {
+            return CharacterAsset.select(color: CharacterAsset.randomColor).image
+        } else {
+            return CharacterAsset.normal(color: CharacterAsset.randomColor).image
+        }
+    }
     
     var body: some View {
-        ZStack {
-            Image("character")
-                .onTapGesture {
-                    isTap.toggle()
+        ScrollView {
+            let columns = [GridItem(.flexible()),
+                           GridItem(.flexible()),
+                           GridItem(.flexible())]
+            LazyVGrid(columns: columns, spacing: 0) {
+                ForEach(0..<16, id: \.self) { _ in
+                    randomImage(isSelect: false)
+                        .onTapGesture {
+                            isTap.toggle()
+                        }
+                        .offset(x: randomHorizontalSpacing, y: randomVerticalSpacing)
+                        .rotationEffect(Angle(degrees: randomRotate))
+                        .frame(height: 146)
+                    if isTap {
+                        PlayerView(nickName: model.nickName, roomType: model.roomType)
+                    }
                 }
-            if isTap {
-                PlayerView(nickName: model.nickName, roomType: .voice)
             }
+            .padding(16)
         }
     }
 }
@@ -40,6 +68,7 @@ struct RollingpaperModel: Identifiable {
     let duration: String?
     let voice: Data?
     let text: String?
+    let roomType: RoomType
     
     init(id: String, nickName: String, duration: String? = nil, voice: Data? = nil, text: String? = nil) {
         self.id = id
@@ -47,5 +76,33 @@ struct RollingpaperModel: Identifiable {
         self.duration = duration
         self.voice = voice
         self.text = text
+        roomType = voice == nil ? .text : .voice
+    }
+}
+
+enum CharacterAsset {
+    case normal(color: CharacterAsset.Color), select(color: CharacterAsset.Color)
+    
+    enum Color: String, CaseIterable {
+        case red = "img_character_1_center_"
+        case yellow = "img_character_2_center_"
+        case green = "img_character_3_center_"
+        case blue = "img_character_4_center_"
+        case lavender = "img_character_5_center_"
+        case lightPink = "img_character_6_center_"
+        case lightGreen = "img_character_7_center_"
+    }
+    
+    var image: Image {
+        switch self {
+        case .normal(let color):
+            return Image("\(color.rawValue)default")
+        case .select(let color):
+            return Image("\(color.rawValue)select")
+        }
+    }
+    
+    static var randomColor: CharacterAsset.Color {
+        CharacterAsset.Color.allCases.randomElement() ?? .lightPink
     }
 }
